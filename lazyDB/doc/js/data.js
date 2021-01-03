bf4b00d54be08a9a393f24e84748130f6335d8f3
Victor Chanet <victor.chanet@yahoo.com>
2021-01-03 12:44:21 +0100

    upadting documentation

--- lazyDB/doc/js/data.js
+++ lazyDB/doc/js/data.js
@@ -4,16 +4,16 @@ let data = [
 			new docObject(["Welcome on BookStorageDB Documentation.", new Warn("LazyDB isn't released yet, you can <a target=\"_blank\" href=\"https://discord.gg/ruzmwh9\">join us on discord</a> to get noticed when it comes out!")])
 		]),
 		new docSection("how it works",[
-			new docObject([	"BookStorageDB is a realtime Database, that means that you can get updates everytime a value is updated ( map, array, number, string , whatever).",el('br'),
+			new docObject([	"LazyDB is a realtime Database, that means that you can get updates everytime a value is updated ( map, array, number, string , whatever).",el('br'),
 							el('br'),
-							"BookStorageDB works with paths, exactly likes the file manager of your OS. Wich means that every value, as a unique path, that is used as an id as well.",el('br'),
+							"LazyDB works with paths, exactly likes the file manager of your OS. Wich means that every value, as a unique path, that is used as an id as well.",el('br'),
 							el('br')])
 		]),
 	new docSection("get started",[
-			new docObject(["To load the client you will need the BookStorageDB Connector. You can download it on our download page for your selected language.", el('br'),],{
+			new docObject(["To load the client you will need the LazyDB Connector. You can download it on our download page for your selected language.", el('br'),],{
 				web : "\
 <head>\n\
-	<script src=\"http://bookstoragedb.com/lib/js/db.js\"></script>\n\
+	<script src=\"http://lazydb.com/lib/js/db.js\"></script>\n\
 </head>\n\
 <body>\n\
 <script>\n\
@@ -39,16 +39,17 @@ db.connect(email, password).then(() => {\n\
 	new docSection("register",[
 		new docObject("Once the user is registered a confirmation email will be sent. All the passwords are automatically encrypted before they are stored.",{
 			js : "\
-db.register(email, password).then(() => {\n\
+db.register(email, full_name, password).then(() => {\n\
+	// You can use full_name as a username too or leave it blank \n\
 	console.log(\"Account successfully created!\");\n\
 }).catch(error => {\n\
 	console.log(error);\n\
 });"
 		})
 	]),
-	new sectionTitle('Manage Data'),
-	new docSection("write data",[
-			new docObject("Write data is really simple in BookStorageDB.",{
+	new sectionTitle('Write Data'),
+	new docSection("set data",[
+			new docObject("Write data is really simple in LazyDB.",{
 				js : "\
 const path = 'towns/bordeaux';\n\
 const data = {\n\
@@ -62,9 +63,42 @@ db.set(path, data).then( function() {\n\
 });"
 			})
 		]),
+	new docSection("create data",[
+		new docObject("Create will fail if the path exist.",{
+			js : "\
+const path = 'towns/bordeaux';\n\
+const data = {\n\
+				desc: 'Amazing city'\n\
+			 };\n\
+			 \n\
+db.create(path, data).then( function() {\n\
+	//data is optional, if you do not specify it, it will create an empty object		\n\
+	console.log('Data updated');\n\
+}).catch(error => {\n\
+	console.log(error);\n\
+});"
+		})
+	]),
+	new docSection("update data",[
+		new docObject("Update will fail if the path does not exist.",{
+			js : "\
+const path = 'towns/bordeaux';\n\
+const data = {\n\
+				desc: 'Amazing city'\n\
+			 };\n\
+			 \n\
+db.set(path, data).then( function() {\n\
+	console.log('Data updated');\n\
+}).catch(error => {\n\
+	console.log(error);\n\
+});"
+		})
+	]),
+
+	new sectionTitle('Read Data'),
 	new docSection("read data once",[
-			new docObject(["To read data once you simply need to run the get command.",el('br')],{
-				js : "\
+		new docObject(["To read data once you simply need to run the get command.",el('br')],{
+			js : "\
 const path = 'towns/bordeaux';\n\
 db.get(path).then(value => {\n\
 	console.log('get value: ' + value);\n\
@@ -72,10 +106,12 @@ db.get(path).then(value => {\n\
 	console.log(error);\n\
 });\
 "
-			})
-		]),
-	new docSection("link data",[
-		new docObject(["link data is usefull when you want to get realtime updates.",el('br')],{
+		})
+
+	]),
+	new sectionTitle('Events'),
+	new docSection("on",[
+		new docObject(["This event is executed each time the data of the specified path changes.",el('br')],{
 			js : "\
 const path = 'towns/bordeaux';\n\
 db.sync(path).then(value => {\n\
@@ -83,16 +119,6 @@ db.sync(path).then(value => {\n\
 }).catch(error => {\n\
 	console.log(error);\n\
 });\
-"
-		})
-	]),
-	new docSection("bind data",[
-		new docObject(["Bind data is really usefull if you want to bind a local variable with a database variable. If you do so everytime one is changed, the other is updated automatically.",el('br'),
-			"To bind a variable you need to chose an object and the prop you want to bind."],
-			{
-			js : "\
-const towns = {'bordeaux' : {}}\n\
-db.bind(towns, 'bordeaux', 'towns/bordeaux')\n\
 "
 		})
 	]),
